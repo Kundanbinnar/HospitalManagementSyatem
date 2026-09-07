4c766ff199d8798b8e64a56e91191d5ff2e26640
Kundan Binnar <kundanbinnar@gmail.com>
2026-09-07 21:32:38 +0530

    Update CORS for production frontend

--- HospitalSystem/src/main/java/com/kundan/config/CorsConfig.java
+++ HospitalSystem/src/main/java/com/kundan/config/CorsConfig.java
@@ -11,7 +11,9 @@ public class CorsConfig implements WebMvcConfigurer{
 	  public void addCorsMappings(CorsRegistry registry) {
 
 	        registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:3000")
+	                .allowedOrigins("http://localhost:3000",
+	                		"https://hospital-management-frontend-production.up.railway.app"
+	                		)
 	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                .allowedHeaders("*");
 	    }
